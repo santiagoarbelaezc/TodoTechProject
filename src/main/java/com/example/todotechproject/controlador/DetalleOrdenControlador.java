dcbad611e50831bb266c82fc15372fe93be2957f
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-04-06 12:25:36 -0500

    actualizacion del controlador detalleOrden

--- src/main/java/com/example/todotechproject/controlador/DetalleOrdenControlador.java
+++ src/main/java/com/example/todotechproject/controlador/DetalleOrdenControlador.java
@@ -1,24 +1,41 @@
 package com.example.todotechproject.controlador;
 
 import com.example.todotechproject.dto.detalleOrden.AgregarProductoDetVenDTO;
+import com.example.todotechproject.dto.detalleOrden.DetalleOrdenDTO;
 import com.example.todotechproject.modelo.entidades.DetalleOrden;
+import com.example.todotechproject.servicios.DetalleOrdenServicios.DetalleOrdenServicio;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/carrito")
+@RequiredArgsConstructor
 public class DetalleOrdenControlador {
 
+    private final DetalleOrdenServicio detalleOrdenServicio;
+
     @PostMapping
-    public void agregarProductoDetOrd(@RequestBody AgregarProductoDetVenDTO detalleProducto) throws  Exception{
-        DetalleOrden detalleOrden = new DetalleOrden();
-        detalleOrden.setProducto(detalleProducto.producto());
-        detalleOrden.setCantidad(detalleProducto.cantidad());
-        detalleOrden.setSubtotal(detalleProducto.subtotal());
+    public ResponseEntity<DetalleOrdenDTO> agregarProductoDetOrd(@RequestBody AgregarProductoDetVenDTO detalleProducto) {
+        Optional<DetalleOrden> optionalDetalle = Optional.ofNullable(detalleOrdenServicio.agregarProductoDetOrd(detalleProducto));
+
+        if (optionalDetalle.isPresent()) {
+            DetalleOrden entidad = optionalDetalle.get();
+            DetalleOrdenDTO dto = new DetalleOrdenDTO(
+                    entidad.getId(),
+                    entidad.getProducto(),
+                    entidad.getCantidad(),
+                    entidad.getSubtotal()
+            );
+            return ResponseEntity.ok(dto);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
