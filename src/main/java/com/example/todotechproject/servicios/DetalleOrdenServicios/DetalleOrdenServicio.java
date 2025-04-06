f049614dcb5dbf559cc783f69333b37767bfb4e9
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-04-06 12:23:44 -0500

    agregacion de metodo del dto

--- src/main/java/com/example/todotechproject/servicios/DetalleOrdenServicios/DetalleOrdenServicio.java
+++ src/main/java/com/example/todotechproject/servicios/DetalleOrdenServicios/DetalleOrdenServicio.java
@@ -1,4 +1,10 @@
 package com.example.todotechproject.servicios.DetalleOrdenServicios;
 
+import com.example.todotechproject.dto.detalleOrden.AgregarProductoDetVenDTO;
+import com.example.todotechproject.modelo.entidades.DetalleOrden;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface DetalleOrdenServicio {
+
+    DetalleOrden agregarProductoDetOrd(AgregarProductoDetVenDTO detalleProducto);
 }
