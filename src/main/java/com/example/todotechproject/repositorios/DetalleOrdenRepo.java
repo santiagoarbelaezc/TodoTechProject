b0a7fec78a5b2259e9b7b2451128e5f0db4cc097
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-04-06 12:23:09 -0500

    agregacion de metodos a la interfaz para consultas en la base de datos

--- src/main/java/com/example/todotechproject/repositorios/DetalleOrdenRepo.java
+++ src/main/java/com/example/todotechproject/repositorios/DetalleOrdenRepo.java
@@ -4,6 +4,17 @@ import com.example.todotechproject.modelo.entidades.DetalleOrden;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DetalleOrdenRepo extends JpaRepository<DetalleOrden, Long> {
+
+    List<DetalleOrden> findByIdProducto(Long productoId);
+
+    List<DetalleOrden> findByIdOrdenVenta(Long ordenVentaId);
+
+    Optional<DetalleOrden> findByOrdenVentaIdAndProductoId(Long ordenVentaId, Long productoId);
+
+
 }
