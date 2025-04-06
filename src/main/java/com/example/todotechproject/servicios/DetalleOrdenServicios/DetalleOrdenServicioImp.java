de287b17df23744c983564b44ee9a86dd91d466d
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-04-06 12:25:00 -0500

    implementacion de interfaz y logica para el dto

--- src/main/java/com/example/todotechproject/servicios/DetalleOrdenServicios/DetalleOrdenServicioImp.java
+++ src/main/java/com/example/todotechproject/servicios/DetalleOrdenServicios/DetalleOrdenServicioImp.java
@@ -1,4 +1,24 @@
 package com.example.todotechproject.servicios.DetalleOrdenServicios;
 
-public class DetalleOrdenServicioImp {
+import com.example.todotechproject.dto.detalleOrden.AgregarProductoDetVenDTO;
+import com.example.todotechproject.modelo.entidades.DetalleOrden;
+import com.example.todotechproject.repositorios.DetalleOrdenRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DetalleOrdenServicioImp implements DetalleOrdenServicio {
+
+    private final DetalleOrdenRepo detalleOrdenRepo;
+
+    @Override
+    public DetalleOrden agregarProductoDetOrd(AgregarProductoDetVenDTO detalleProducto) {
+        DetalleOrden detalleOrden = new DetalleOrden();
+        detalleOrden.setProducto(detalleProducto.producto());
+        detalleOrden.setCantidad(detalleProducto.cantidad());
+        detalleOrden.setSubtotal(detalleProducto.subtotal());
+
+        return detalleOrdenRepo.save(detalleOrden);
+    }
 }
