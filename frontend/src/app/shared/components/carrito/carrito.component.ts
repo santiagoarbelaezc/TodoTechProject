679134f02fa4a164f9d429715956e1ae83e81d70
Maria Jose M <ingmariamarmolejo@gmail.com>
2025-05-03 22:57:15 -0500

    agregando totales carrito de compras

--- frontend/src/app/shared/components/carrito/carrito.component.ts
+++ frontend/src/app/shared/components/carrito/carrito.component.ts
@@ -1,6 +1,6 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 import {UsuarioService} from "../../../services/usuario.service";
-import {NgClass, NgFor, NgIf} from "@angular/common";
+import {DecimalPipe, NgClass, NgFor, NgIf} from "@angular/common";
 import {CarritoService} from "../../../services/carrito.service";
 import {Subscription} from "rxjs";
 import {DetalleOrdenDto} from "../../../models/detalle.orden.dto";
@@ -10,13 +10,16 @@ import {DetalleOrdenDto} from "../../../models/detalle.orden.dto";
     templateUrl: './carrito.component.html',
     styleUrl: './carrito.component.css',
     standalone: true,
-    imports: [NgClass, NgIf, NgFor]
+    imports: [NgClass, NgIf, NgFor, DecimalPipe]
 })
 export class CarritoComponent implements OnInit {
     mostrarCarrito = false; // Controla si el carrito aparece al bajar
     carritoVisible = false; // Controla si el carrito está desplegado
     carritoItems: DetalleOrdenDto[] = []; // Lista de productos en el carrito
+    total : number = 0.0;
+    totalItems : number =0;
     private carritoSubscription!: Subscription;
+
 
 
     constructor(public authService: UsuarioService, public carritoService: CarritoService) {
@@ -39,10 +42,19 @@ export class CarritoComponent implements OnInit {
         // Sincroniza la lista local con el carrito del servicio
         this.carritoSubscription = this.carritoService.getCarrito().subscribe((items: DetalleOrdenDto[]) => {
             this.carritoItems = items;
+            this.totalCarrito();
         }, (error: any) => {
             console.error('Error al obtener el carrito:', error);
         });
     }
 
+    // Calculate the total price of the cart items
+    totalCarrito(): void {
+        this.total =  this.carritoItems.reduce((total, item) => total + item.subtotal, 0);
+        this.totalItems = this.carritoItems.reduce((total, item) => total + item.cantidad, 0);
+    }
 
+    crearOrden() {
+
+    }
 }
